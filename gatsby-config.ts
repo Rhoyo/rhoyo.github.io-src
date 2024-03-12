3f87c73cad0f29bada6c042452c115a10fe0eccb
Rhoyo <ryanhkho@gmail.com>
2024-03-11 17:01:23 -1000

    dev

---
+++ gatsby-config.ts
@@ -0,0 +1,19 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `rhoyo`,
+    siteUrl: `https://www.rhoyo.github.io`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: ["gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
+export default config;
