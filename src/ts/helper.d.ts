0e90c9332ef0138480dbc10d541edf89496dc0b4
Ryan Ho <ryanhkho@gmail.com>
2024-08-25 15:20:29 -1000

    add waveHelper.ts and helper.d.ts

---
+++ src/ts/helper.d.ts
@@ -0,0 +1,5 @@
+
+declare module '../js/waveHelper' {
+    export function createWave(radiusTop: number, radiusBottom: number, radiusX: number, radiusY: number, height: number, radialSegments: number, heightSegments: number, thetaStart: number, thetaLength: number): THREE.Mesh;
+    export function createParticles(radiusX: number, radiusY: number, height: number, thetaLength: number): ThisParameterType.Mesh;
+}
